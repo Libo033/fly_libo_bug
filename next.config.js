060c67220c1cb8d22c90ba59d6cbe01e7944fd25
Libo033 <113865453+Libo033@users.noreply.github.com>
2023-09-21 11:51:48 -0300

    Cambio de imagenes en nextconfig

--- next.config.js
+++ next.config.js
@@ -9,18 +9,6 @@ const nextConfig = {
         port: "",
         pathname: "/dsuydyqgz/**",
       },
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-        port: "",
-        pathname: "",
-      },
-      {
-        protocol: "https",
-        hostname: "graph.facebook.com",
-        port: "",
-        pathname: "",
-      },
     ],
   },
 }
